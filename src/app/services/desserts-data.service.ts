072f10815d2094aefe9c3015348d1a69d6b5a4cb
Marc Bernini <bernini.marc@gmail.com>
2019-06-04 16:24:10 +0200

    Service to get desserts data from API created

---
+++ src/app/services/desserts-data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DessertsDataService {
+
+  dessertsRoute = 'desserts';
+
+  constructor(private http: HttpClient) { }
+
+  getDesserts(): Observable<object> {
+    return this.http.get(this.dessertsRoute);
+  }
+}

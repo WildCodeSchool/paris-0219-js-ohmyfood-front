bc1a29475986f81bb713a0fadc842a00b4a71041
Marc Bernini <bernini.marc@gmail.com>
2019-06-04 16:25:40 +0200

    Import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { DessertsComponent } from './components/desserts/desserts.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
 
 @NgModule({
   declarations: [
@@ -11,8 +12,9 @@ import { DessertsComponent } from './components/desserts/desserts.component';
     DessertsComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

fe4b69164142a624463f676122a4a1737d038bf8
Marc Bernini <bernini.marc@gmail.com>
2019-06-04 15:37:41 +0200

    Component directory and dessertComponent created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DessertsComponent } from './components/desserts/desserts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DessertsComponent
   ],
   imports: [
     BrowserModule,

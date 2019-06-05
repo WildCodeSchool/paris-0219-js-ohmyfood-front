cd1cbfad685f8fae4bd1f4484821f8ac4bd110b7
Jeremy-san <shehata.jeremy@gmail.com>
2019-06-05 14:50:00 +0200

    correct component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SaladsSaucesComponent } from './component/salads-sauces/salads-sauces.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SaladsSaucesComponent
+
+
   ],
   imports: [
     BrowserModule,

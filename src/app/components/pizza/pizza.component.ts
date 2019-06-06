a8b4293be471645adf87bdcc24a3b1824d0d5313
Triple <sebastien.andrys@gmail.com>
2019-06-06 17:25:49 +0200

    reactive form in progress + cycle problem with get functions

--- src/app/components/pizza/pizza.component.ts
+++ src/app/components/pizza/pizza.component.ts
@@ -7,29 +7,38 @@ import { PizzaService } from 'src/app/services/pizza.service';
   templateUrl: './pizza.component.html',
   styleUrls: ['./pizza.component.scss']
 })
-export class PizzaComponent {
+export class PizzaComponent implements OnInit {
   formSubmit = false;
   regexPrice = /[0-9]+[.]+[0-9]*/gm
-  pizzaForm = new FormGroup({
-    pizzaName: new FormControl('', [Validators.required, Validators.maxLength(45)]),
-    pizzDesc: new FormControl('', [Validators.required, Validators.maxLength(255)]),
-    pizzPriceHt: new FormControl('', [Validators.required, Validators.maxLength(6), Validators.pattern(this.regexPrice)]),
-    idTax: new FormControl('')
-  })
-
   pizzaFormObject;
+  pizzaForm;
+
   constructor(private pizzaService: PizzaService) { }
+
+  ngOnInit() {
+    this.pizzaForm = new FormGroup({
+      pizzaName: new FormControl('', [Validators.required, Validators.maxLength(45)]),
+      pizzDesc: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+      pizzPriceHt: new FormControl('', [Validators.required, Validators.maxLength(6), Validators.pattern(this.regexPrice)]),
+      idTax: new FormControl('')
+    });
+  }
+
+  get fPizzaName() { return this.pizzaForm.get('pizzaName') }
+  get fPizzDesc() { return this.pizzaForm.get('pizzDesc') }
+  get fPizzPriceHt() { return this.pizzaForm.get('pizzPriceHt') }
+  get fIdTax() { return this.pizzaForm.get('idTax') }
 
   onSubmit() {
     this.formSubmit = true;
-    this.pizzaFormObject = {
-        pizzName: this.pizzaForm.value.pizzaName, 
-        pizzDesc: this.pizzaForm.value.pizzDesc, 
-        pizzPriceHt: parseFloat(this.pizzaForm.value.pizzPriceHt), 
-        idTax: this.pizzaForm.value.idTax
-    }
-    
-    this.pizzaService.pizzaFormObject = this.pizzaFormObject;
+    if (this.pizzaForm.valid) {
+     /* this.pizzaService.pizzaFormObject = {
+        pizzName: this.fPizzaName, 
+        pizzDesc: this.fPizzDesc, 
+        pizzPriceHt: parseFloat(this.fPizzPriceHt), 
+        idTax: this.fIdTax
+      };*/
+    };
     this.pizzaService.addPizzaType().subscribe(data => data);
   }
 }

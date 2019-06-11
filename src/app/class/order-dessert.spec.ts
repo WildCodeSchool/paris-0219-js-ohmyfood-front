c180ea1d25469a4a8654d644d05b488232da303c
Marc Bernini <bernini.marc@gmail.com>
2019-06-11 14:05:44 +0200

    Class created for dessert's User choice

---
+++ src/app/class/order-dessert.spec.ts
@@ -0,0 +1,7 @@
+import { OrderDessert } from './order-dessert';
+
+describe('OrderDessert', () => {
+  it('should create an instance', () => {
+    expect(new OrderDessert()).toBeTruthy();
+  });
+});

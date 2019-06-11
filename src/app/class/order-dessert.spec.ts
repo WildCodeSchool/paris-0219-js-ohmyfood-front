2cefa27641cd009d0c778b75be22cce0a6613994
Marc Bernini <bernini.marc@gmail.com>
2019-06-11 15:14:12 +0200

    delete spec.ts because useless

--- src/app/class/order-dessert.spec.ts
+++
@@ -1,7 +0,0 @@
-import { OrderDessert } from './order-dessert';
-
-describe('OrderDessert', () => {
-  it('should create an instance', () => {
-    expect(new OrderDessert()).toBeTruthy();
-  });
-});

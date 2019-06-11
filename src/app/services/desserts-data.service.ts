80f66a3d35301c461fca21876919452eb8a6c81a
Marc Bernini <bernini.marc@gmail.com>
2019-06-11 14:13:51 +0200

    Add method to create object with orderDessert class and a method to sort user's choice array

--- src/app/services/desserts-data.service.ts
+++ src/app/services/desserts-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { OrderDessert } from '../class/order-dessert';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,41 @@ export class DessertsDataService {
 
   dessertsRoute = 'http://localhost:3000/desserts';
 
+  userChoice: Array<OrderDessert> = [];
+
   constructor(private http: HttpClient) { }
 
   getDesserts(): Observable<object> {
     return this.http.get(this.dessertsRoute);
   }
+
+  createOrderDessert(formResult) { // create object with OrderDessert Class
+    for (const key in formResult) {
+      if (formResult.hasOwnProperty(key)) {
+        formResult[key].map(test => {
+          if (test.dessQuantity > 0) {
+            const choice = new OrderDessert(test.idDesserts, test.dessName, test.dessPriceTTC * test.dessQuantity, test.dessQuantity);
+            this.userChoice.push(choice);
+            }
+          }
+        );
+      }
+    }
+    if (this.userChoice.length > 1) {
+      this.sortUserChoice();
+    }
+    console.log(this.userChoice);
+  }
+
+  sortUserChoice() { // Remove duplicate choice
+    for (let i = 0; i < this.userChoice.length; i ++) {
+      for (let j = i + 1 ; j < this.userChoice.length; j ++ ) {
+        if (this.userChoice[i].dessName === this.userChoice[j].dessName) {
+          this.userChoice[i].dessPrice *= 2;
+          this.userChoice.splice(j, 1);
+        }
+      }
+    }
+  }
+
 }

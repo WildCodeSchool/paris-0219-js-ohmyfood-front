9f565da2911a8490399cdcf51bac327a813bb1da
Marc Bernini <bernini.marc@gmail.com>
2019-06-13 15:58:15 +0200

    Change name of dessertsComponent to dessertsFormComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { DessertsComponent } from './components/desserts/desserts.component';
+import { DessertsFormComponent } from './components/desserts-form/desserts-form.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
@@ -14,7 +14,7 @@ import { PizzasFormComponent } from './components/pizzas-form/pizzas-form.compon
 @NgModule({
   declarations: [
     AppComponent,
-    DessertsComponent,
+    DessertsFormComponent,
     PizzaComponent,
     PizzaPageComponent,
     PizzasFormComponent

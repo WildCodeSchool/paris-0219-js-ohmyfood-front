f40a121d44f6e3fc95d12e69a37bd85de1ed92c3
Marc Bernini <bernini.marc@gmail.com>
2019-06-13 16:01:49 +0200

    Service created to get data from pizzas table, to create orderPizzas object and to sort the array of user's choice

--- src/app/services/pizzas-data.service.ts
+++ src/app/services/pizzas-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { OrderPizzas } from '../class/order-pizzas';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,40 @@ export class PizzasDataService {
 
   pizzasRoute = 'http://localhost:3000/pizzas';
 
+  userChoice: Array<OrderPizzas> = [];
+
   constructor(private http: HttpClient) { }
 
   getPizzas(): Observable<object> {
     return this.http.get(this.pizzasRoute);
   }
 
+  createOrderPizzas(formResult) { // create object with OrderDessert Class
+    for (const key in formResult) {
+      if (formResult.hasOwnProperty(key)) {
+        formResult[key].map(test => {
+          if (test.pizzQuantity > 0) {
+            const choice = new OrderPizzas(test.idPizzas, test.pizzName, test.pizzPriceTTC * test.pizzQuantity, test.pizzQuantity);
+            this.userChoice.push(choice);
+            }
+          }
+        );
+      }
+    }
+    if (this.userChoice.length > 1) {
+      this.sortUserChoice();
+    }
+  }
+
+  sortUserChoice() { // Remove duplicate choice
+    for (let i = 0; i < this.userChoice.length; i ++) {
+      for (let j = i + 1 ; j < this.userChoice.length; j ++ ) {
+        if (this.userChoice[i].pizzName === this.userChoice[j].pizzName) {
+          this.userChoice[i].pizzPrice += this.userChoice[j].pizzPrice; // Sum of price
+          this.userChoice[i].pizzQuantity += this.userChoice[j].pizzQuantity; // Sum of quantity
+          this.userChoice.splice(j, 1); // Remove duplicate
+        }
+      }
+    }
+  }
 }

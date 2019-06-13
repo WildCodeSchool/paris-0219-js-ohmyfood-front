7765991593c63065792463bd788d3087010b2e4e
Marc Bernini <bernini.marc@gmail.com>
2019-06-13 16:02:36 +0200

    pizzasForm component created to display form pizzas, like form desserts

--- src/app/components/pizzas-form/pizzas-form.component.ts
+++ src/app/components/pizzas-form/pizzas-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzasDataService } from 'src/app/services/pizzas-data.service';
+import { FormArray, FormBuilder } from '@angular/forms';
+import { QuantitySelectService } from 'src/app/services/quantity-select.service';
 
 @Component({
   selector: 'app-pizzas-form',
@@ -10,20 +12,73 @@ export class PizzasFormComponent implements OnInit {
 
   pizzasList: object;
 
-  constructor(private pizzasData: PizzasDataService) { }
+  enableSubmit: boolean;
+
+  formPizzas = this.formBuilder.group({
+    selectedPizzas: this.formBuilder.array([])
+  });
+
+  constructor(
+    private pizzasData: PizzasDataService,
+    private formBuilder: FormBuilder,
+    private quantitySelectService: QuantitySelectService
+    ) { }
 
   ngOnInit() {
     this.pizzasData.getPizzas()
     .subscribe(pizzas => {
       this.pizzasList = pizzas;
-      console.log(this.pizzasList);
 
       for (const key in this.pizzasList) {
         if (this.pizzasList.hasOwnProperty(key)) {
           this.pizzasList[key].pizzPriceTTC = this.pizzasList[key].pizzPriceTTC.toFixed(2);
         }
       }
+
+      const selectedPizzas = this.formPizzas.get('selectedPizzas') as FormArray;
+
+      for (const key in this.pizzasList) {
+        if (this.pizzasList.hasOwnProperty(key)) {
+          const pizzasForm = this.formBuilder.group({
+            idPizzas: [ this.pizzasList[key].idPizzas ],
+            pizzName: [ this.pizzasList[key].pizzName ],
+            pizzPriceTTC: [ this.pizzasList[key].pizzPriceTTC ],
+            pizzQuantity: [0]
+          });
+          selectedPizzas.push(pizzasForm);
+        }
+      }
     });
+  }
+
+  onSubmit() {
+    const orderPizzas = this.formPizzas.value;
+
+    this.pizzasData.createOrderPizzas(orderPizzas);
+
+    this.resetFormDessert();
+
+    this.enableSubmit = false;
+  }
+
+  quantitySelect(operator, i, quantity) {
+    this.formPizzas.value.selectedPizzas[i].pizzQuantity = this.quantitySelectService.selectQuantity(operator, quantity);
+
+    if (this.formPizzas.value.selectedPizzas[i].pizzQuantity > 0) {
+      this.enableSubmit = true;
+    } else {
+      this.enableSubmit = false;
+    }
+  }
+
+  resetFormDessert() {
+    for (const key in this.formPizzas.value) {
+      if (this.formPizzas.value.hasOwnProperty(key)) {
+        this.formPizzas.value[key].map(
+          resetQuantity => resetQuantity.pizzQuantity = 0
+          );
+      }
+    }
   }
 
 }

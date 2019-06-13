a58af3704b7a6370f986ca1678fb597dca8fa244
Arthur Baudel <arthurbaudel75.com>
2019-06-13 09:41:21 +0200

    .ts is good

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
-}
+  @ViewChild('navBurger', {static: true}) navBurger: ElementRef<any>;
+  @ViewChild('navMenu', {static: true}) navMenu: ElementRef<any>;
 
+  toggleNavbar() {
+      this.navBurger.nativeElement.classList.toggle('is-active');
+      this.navMenu.nativeElement.classList.toggle('is-active');
+  }
+
+}

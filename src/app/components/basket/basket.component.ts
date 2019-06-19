45ebe72da25894c290ad91f4ee4fe98077d9c8ac
Marc Bernini <bernini.marc@gmail.com>
2019-06-19 18:56:27 +0200

    Add reactive form to display userChoice and give him the possibility to change quantity in basketComponent. We have the same structure of other form with buttons - and +

--- src/app/components/basket/basket.component.ts
+++ src/app/components/basket/basket.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PizzasDataService } from 'src/app/services/pizzas-data.service';
 import { OrderPizzas } from 'src/app/class/order-pizzas';
 import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { QuantitySelectService } from 'src/app/services/quantity-select.service';
 
 @Component({
   selector: 'app-basket',
@@ -16,6 +17,8 @@ export class BasketComponent implements OnInit {
 
   totalArray: Array<number> = []; // Use to calculate total price
 
+  initPrice: boolean;
+
   total: number; // final result
 
   finalOrderForm = this.formBuilder.group({
@@ -26,14 +29,13 @@ export class BasketComponent implements OnInit {
 
   constructor(
     private pizzasData: PizzasDataService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private quantityService: QuantitySelectService
     ) { }
 
   ngOnInit() {
     this.pizzasData.getUserPizzas.subscribe(pizzasChoice => {
       this.userPizzaChoice = pizzasChoice;
-
-      console.log(this.userPizzaChoice);
 
       const pizza = this.finalOrderForm.get('pizza') as FormArray;
 
@@ -42,28 +44,25 @@ export class BasketComponent implements OnInit {
           const pizzChoice = this.formBuilder.group({
             idPizzas: [this.userPizzaChoice[key].idPizzas],
             pizzName: [this.userPizzaChoice[key].pizzName],
-            pizzPriceTotal: [this.userPizzaChoice[key].pizzPrice],
+            pizzPriceTotal: [this.userPizzaChoice[key].pizzPrice * this.userPizzaChoice[key].pizzQuantity],
             pizzQuantity: [this.userPizzaChoice[key].pizzQuantity]
           });
           pizza.push(pizzChoice);
         }
       }
 
-      if (pizza.value.length > 1) {
-        for (let i = 0; i < pizza.value.length; i ++) {
-          for (let j = i + 1 ; j < pizza.value.length; j ++ ) {
-            if (pizza.value[i].pizzName === pizza.value[j].pizzName) {
-              pizza.value[i].pizzPriceTotal = 0;
-              pizza.value[i].pizzQuantity = 0;
-              pizza.value[i].pizzPriceTotal += pizza.value[j].pizzPriceTotal; // Sum of price
-              pizza.value[i].pizzQuantity += pizza.value[j].pizzQuantity; // Sum of quantity
-              pizza.removeAt(j); // Remove duplicate
-            }
+      for (let i = 0; i < pizza.value.length; i ++) {
+        for (let j = i + 1 ; j < pizza.value.length; j ++ ) {
+          if (pizza.value[i].pizzName === pizza.value[j].pizzName) {
+            pizza.value[i].pizzPriceTotal = 0;
+            pizza.value[i].pizzQuantity = 0;
+            pizza.value[i].pizzPriceTotal += pizza.value[j].pizzPriceTotal; // Sum of price
+            pizza.value[i].pizzQuantity += pizza.value[j].pizzQuantity; // Sum of quantity
+            pizza.removeAt(j); // Remove duplicate
           }
         }
       }
 
-      console.log(this.finalOrderForm);
       const reducer = (accumulator, currentValue) => accumulator + currentValue; // Sum of array's value function
 
       for (const key in this.userPizzaChoice) {
@@ -93,6 +92,22 @@ export class BasketComponent implements OnInit {
   onSubmit() {
     const finalOrder = this.finalOrderForm.value;
     console.log(finalOrder);
+  }
+
+  quantitySelect(operator, index, quantity) {
+    quantity = this.finalOrderForm.value.pizza[index].pizzQuantity = this.quantityService.selectQuantity(operator, quantity);
+    this.finalOrderForm.value.pizza[index].pizzPriceTotal =
+    this.quantityService.updatePrice(this.userPizzaChoice[index].pizzPrice, quantity);
+
+    const pizza = this.finalOrderForm.get('pizza') as FormArray;
+    if (pizza.controls[index].value.pizzQuantity === 0) {
+      console.log(pizza);
+      pizza.controls[index].patchValue({
+        pizzQuantity: 0
+      });
+      pizza.removeAt(index);
+      console.log(this.finalOrderForm);
+    }
   }
 
 }

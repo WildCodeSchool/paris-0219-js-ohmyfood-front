381e9032bb3f7f47ffdb95355837d4f4993504a0
Marc Bernini <bernini.marc@gmail.com>
2019-06-20 11:38:24 +0200

    Change key name of formArray of pizzas to match with database. Add an else if to create form according to object class

--- src/app/services/create-form-basket.service.ts
+++ src/app/services/create-form-basket.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { OrderPizzas } from '../class/order-pizzas';
+import { OrderBeverage } from '../class/order-beverage';
+import { OrderDessert } from '../class/order-dessert';
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +12,41 @@ export class CreateFormBasketService {
   constructor(private fb: FormBuilder) { }
 
   createOrderForm(formToCreate) {
-    const pizzChoice = this.fb.group({
-      idPizzas: [formToCreate.idPizzas],
-      pizzName: [formToCreate.pizzName],
-      pizzPriceTotal: [formToCreate.pizzPrice * formToCreate.pizzQuantity],
-      pizzQuantity: [formToCreate.pizzQuantity]
-    });
-    return pizzChoice;
+    if (formToCreate instanceof OrderPizzas) {
+      const pizzChoice = this.fb.group({
+        idPizzas: [formToCreate.idPizzas],
+        pizzasName: [formToCreate.pizzName],
+        pizzasPriceTotal: [formToCreate.pizzPrice * formToCreate.pizzQuantity],
+        pizzasQuantity: [formToCreate.pizzQuantity]
+      });
+      return pizzChoice;
+
+    } else if (formToCreate instanceof OrderBeverage) {
+      const drinksChoice = this.fb.group({
+        idBeverages: [formToCreate.idBeverages],
+        bevName: [formToCreate.bevName],
+        bevPriceTotal: [formToCreate.bevPrice * formToCreate.bevQuantity],
+        bevQuantity: [formToCreate.bevQuantity]
+      });
+      return drinksChoice;
+
+    } else if (formToCreate instanceof OrderDessert) {
+      const dessChoice = this.fb.group({
+        idDesserts: [formToCreate.idDesserts],
+        dessertsName: [formToCreate.dessName],
+        dessPriceTotal: [formToCreate.dessPrice * formToCreate.dessQuantity],
+        dessQuantity: [formToCreate.dessQuantity]
+      });
+      return dessChoice;
+    }
   }
 
   sortOrderForm(formToSort, i, j) {
-    formToSort.value[i].pizzPriceTotal = 0;
-    formToSort.value[i].pizzQuantity = 0;
+    formToSort.value[i].pizzasPriceTotal = 0;
+    formToSort.value[i].pizzasQuantity = 0;
 
-    formToSort.value[i].pizzPriceTotal += formToSort.value[j].pizzPriceTotal; // Sum of price
-    formToSort.value[i].pizzQuantity += formToSort.value[j].pizzQuantity; // Sum of quantity
+    formToSort.value[i].pizzasPriceTotal += formToSort.value[j].pizzasPriceTotal; // Sum of price
+    formToSort.value[i].pizzasQuantity += formToSort.value[j].pizzasQuantity; // Sum of quantity
     formToSort.removeAt(j); // Remove duplicate
     return formToSort;
   }

ef45cd3dca5f3f404ab24940104389f19973d870
Marc Bernini <bernini.marc@gmail.com>
2019-07-01 17:00:28 +0200

    Modification of this validator to have better control. User can submit if he choose one or two bases maximum

--- src/app/validators/saladsBaseValidators.ts
+++ src/app/validators/saladsBaseValidators.ts
@@ -3,11 +3,14 @@ import { AbstractControl, ValidatorFn, ValidationErrors} from '@angular/forms';
 export function checkSaladsBase(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
 
-    const checkBox = control.value.map(value => value.saladsBaseQuantity)
-      .filter(newValue => newValue === true);
+    const checkBox = control.value.map(
+      value => value.saladsBaseQuantity
+      ).filter(
+        newValue => newValue === true
+        );
 
-    if (checkBox.length > 2) {
-        return { tooMuchBase: 'Merci de ne sélectionner que deux bases maximum' };
+    if (checkBox.length === 0 || checkBox.length > 2) {
+        return { tooMuchBase: 'Merci de sélectionner au minimum une base. Deux bases maximum sont possibles.' };
     } else {
         return null;
     }

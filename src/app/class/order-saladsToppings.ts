735079ea9556f1d3b37eca5016aa0eaa1e3b2c4a
Marc Bernini <bernini.marc@gmail.com>
2019-07-02 17:48:56 +0200

    Change some key in this class to adapt it with database

--- src/app/class/order-saladsToppings.ts
+++ src/app/class/order-saladsToppings.ts
@@ -1,6 +1,6 @@
 export class SaladsToppings {
   constructor(
-    public idSaladsToppongs: number,
+    public idSaladsToppings: number,
     public saladsToppingsName: string,
     public saladsToppingsPrice: string,
     public saladsToppingsQuantity: number

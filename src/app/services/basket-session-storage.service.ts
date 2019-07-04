1b6e1aca66716d71df6c22c0a6c4370ea9feca72
Marc Bernini <bernini.marc@gmail.com>
2019-07-04 19:04:48 +0200

    Change method to make difference of argument to save in sessionStorage good item

--- src/app/services/basket-session-storage.service.ts
+++ src/app/services/basket-session-storage.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { OrderDessert } from '../class/order-dessert';
+import { OrderBeverage } from '../class/order-beverage';
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +9,30 @@ export class BasketSessionStorageService {
 
   constructor() { }
 
-  saveToLocalStorage(reset: string, form) {
-    sessionStorage.setItem('beverages', JSON.stringify(form.map(
-      (bev: Array<object>) => bev)
-      )
-    );
+  saveToSessionStorage(reset: string, form: object[]) {
+    if (form.length > 0) {
+      const check = Object.getOwnPropertyNames(form[0]);
+      const finalCheck = check[0];
+
+      switch (finalCheck) {
+        case 'idBeverages' :
+          sessionStorage.setItem('beverages', JSON.stringify(form.map(
+            (bev: OrderBeverage[]) => bev)
+            )
+          );
+          break;
+
+        case 'idDesserts' :
+          sessionStorage.setItem('desserts', JSON.stringify(form.map(
+            (dess: OrderDessert[]) => dess)
+            )
+          );
+      }
+    }
+
     if (reset === 'reset') {
-    sessionStorage.removeItem('beverages');
+      sessionStorage.removeItem('beverages');
+      sessionStorage.removeItem('desserts');
+    }
   }
-}
 }

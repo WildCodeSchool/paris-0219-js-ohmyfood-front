3aeeb57bc6f43a817f96bf589080ed22a81c9ddd
Marc Bernini <bernini.marc@gmail.com>
2019-07-05 10:42:50 +0200

    Add method specific to clear session storage according to user choice

--- src/app/services/basket-session-storage.service.ts
+++ src/app/services/basket-session-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { OrderDessert } from '../class/order-dessert';
 import { OrderBeverage } from '../class/order-beverage';
+import { OrderPizzas } from '../class/order-pizzas';
 
 @Injectable({
   providedIn: 'root'
@@ -9,30 +10,59 @@ export class BasketSessionStorageService {
 
   constructor() { }
 
-  saveToSessionStorage(reset: string, form: object[]) {
+  saveToSessionStorage(form: object[]) {
+
     if (form.length > 0) {
       const check = Object.getOwnPropertyNames(form[0]);
       const finalCheck = check[0];
 
       switch (finalCheck) {
-        case 'idBeverages' :
-          sessionStorage.setItem('beverages', JSON.stringify(form.map(
-            (bev: OrderBeverage[]) => bev)
-            )
-          );
-          break;
+      case 'idPizzas' :
+        sessionStorage.setItem('pizzas', JSON.stringify(form.map(
+          (pizz: OrderPizzas[]) => pizz)
+          )
+        );
+        break;
 
-        case 'idDesserts' :
-          sessionStorage.setItem('desserts', JSON.stringify(form.map(
-            (dess: OrderDessert[]) => dess)
-            )
-          );
+      case 'idBeverages' :
+        sessionStorage.setItem('beverages', JSON.stringify(form.map(
+          (bev: OrderBeverage[]) => bev)
+          )
+        );
+        break;
+
+      case 'idDesserts' :
+        sessionStorage.setItem('desserts', JSON.stringify(form.map(
+          (dess: OrderDessert[]) => dess)
+          )
+        );
+        break;
+      default:
+      return null;
       }
     }
+  }
 
-    if (reset === 'reset') {
+
+  clearSessionStorage(form: any) {
+    // Remove all if user click on reset basket
+    if (form.length === 0) {
+      sessionStorage.removeItem('pizzas');
       sessionStorage.removeItem('beverages');
       sessionStorage.removeItem('desserts');
+    } else {
+        // Clear session storage according to user's choice
+        if (form.pizza.length === 0) {
+          sessionStorage.removeItem('pizzas');
+        }
+
+        if (form.beverage.length === 0) {
+          sessionStorage.removeItem('beverages');
+        }
+
+        if (form.dessert.length === 0) {
+          sessionStorage.removeItem('desserts');
+        }
     }
   }
 }

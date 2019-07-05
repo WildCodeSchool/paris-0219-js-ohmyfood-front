420c1a8a05c6b84a5bf028e22f831b8ab1865bfd
Marc Bernini <bernini.marc@gmail.com>
2019-07-05 19:24:26 +0200

    Subscribe to Output from finalOrderService. Save data in session storage if it's empty. On the nbOnInit, if there is data in sessionStorage, component get it.

--- src/app/components/detail-order/detail-order.component.ts
+++ src/app/components/detail-order/detail-order.component.ts
@@ -7,6 +7,8 @@ import { OrderBeverage } from 'src/app/class/order-beverage';
 import { BeveragesDataService } from 'src/app/services/beverages-data.service';
 import { OrderDessert } from 'src/app/class/order-dessert';
 import { DessertsDataService } from 'src/app/services/desserts-data.service';
+import { FinalOrder } from 'src/app/class/final-order';
+import { FinalOrderService } from 'src/app/services/final-order.service';
 
 @Component({
   selector: 'app-detail-order',
@@ -15,12 +17,14 @@ import { DessertsDataService } from 'src/app/services/desserts-data.service';
 })
 export class DetailOrderComponent implements OnInit {
 
-  orderStatus: "Livraison"
+  orderStatus: 'Livraison';
   enableSubmit: boolean;
 
   userPizzaChoice: Array<OrderPizzas>; // Get data from service
   userBeveragesChoice: Array<OrderBeverage>;
   userDessertsChoice: Array<OrderDessert>;
+
+  finalOrderRecap: FinalOrder;
 
   initPrice: boolean;
 
@@ -31,11 +35,78 @@ export class DetailOrderComponent implements OnInit {
     private beverageData: BeveragesDataService,
     private dessertData: DessertsDataService,
     private quantityService: QuantitySelectService,
-    private createForm: CreateFormBasketService
+    private createForm: CreateFormBasketService,
+    private finalOrder: FinalOrderService
   ) { }
 
   ngOnInit() {
-    // Récupération du localStorage
+    if (sessionStorage.getItem('finalOrder')) {
+      this.finalOrderRecap = JSON.parse(sessionStorage.getItem('finalOrder'));
+    }
+
+    this.finalOrder.getFinalOrder.subscribe((userFinalOrder: any) => {
+      if (!sessionStorage.getItem('finalOrder')) {
+        sessionStorage.setItem('finalOrder', JSON.stringify(userFinalOrder));
+        this.finalOrderRecap = userFinalOrder;
+      } else {
+        const finalOrderStorage = JSON.parse(sessionStorage.getItem('finalOrder'));
+        const finalOrderTransition = {
+          pizza: [],
+          salad: [],
+          beverage: [],
+          dessert: [],
+        };
+        for (const key of Object.entries(userFinalOrder)) {
+          switch (key[0]) {
+            case 'pizza' :
+              finalOrderTransition.pizza.push(key[1]);
+              break;
+            case 'salad' :
+              finalOrderTransition.salad.push(key[1]);
+              break;
+            case 'beverage' :
+              finalOrderTransition.beverage.push(key[1]);
+              break;
+            case 'dessert' :
+              finalOrderTransition.dessert.push(key[1]);
+              break;
+            default: return null;
+          }
+        }
+        for (const key of Object.entries(finalOrderStorage)) {
+          switch (key[0]) {
+            case 'pizza' :
+              finalOrderTransition.pizza.push(key[1]);
+              break;
+            case 'salad' :
+              finalOrderTransition.salad.push(key[1]);
+              break;
+            case 'beverage' :
+              finalOrderTransition.beverage.push(key[1]);
+              break;
+            case 'dessert' :
+              finalOrderTransition.dessert.push(key[1]);
+              break;
+            default: return null;
+          }
+        }
+
+        let test = [... finalOrderTransition.pizza['0']];
+        const test2 = [... finalOrderTransition.pizza['1']];
+        test = test.map(a => {
+          return test2.map(y => {
+            if (a.idPizzas === y.idPizzas) {
+              a.pizzasPriceTotal += y.pizzasPriceTotal;
+              a.pizzasQuantity += y.pizzasQuantity;
+              return a;
+            }
+          });
+        });
+        console.log(test);
+
+        console.log(test2);
+      }
+    });
   }
 }
 

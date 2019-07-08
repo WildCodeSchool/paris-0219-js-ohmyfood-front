95c6e2e5b711ab674e45383f28c27efc3d3f3313
Marc Bernini <bernini.marc@gmail.com>
2019-07-08 20:10:27 +0200

    Add method to create instance of menuPizza class according to user choice and emit it to basketComponent

--- src/app/services/menu-prices-data.service.ts
+++ src/app/services/menu-prices-data.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { MenuPizza } from '../class/menu-pizza';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,28 @@ export class MenuPricesDataService {
 
   menuPricesRoute = 'http://localhost:3000/menuPrices';
 
+  @Output()
+  getMenuPizza: EventEmitter<any> = new EventEmitter();
+
   constructor(private http: HttpClient) { }
 
   getMenuPrices(): Observable<object> {
     return this.http.get(this.menuPricesRoute);
+  }
+
+  createOrderMenu(userMenuChoice: object) {
+    // Get each choice. Sort form result
+    const pizza = userMenuChoice[`pizza`].filter((userPizza: any) => userPizza.pizzQuantity > 0);
+    const beverage = userMenuChoice[`beverage`].filter((userBeverage: any) => userBeverage.bevQuantity > 0);
+    const dessert = userMenuChoice[`dessert`].filter((userDessert: any) => userDessert.dessQuantity > 0);
+
+    // Create instance of MenuPizza
+    const menuPizza = new MenuPizza(
+      pizza[0],
+      beverage[0],
+      dessert[0],
+      userMenuChoice[`pizzaMenuPrice`].pizzaMenuPrice
+    );
+    this.getMenuPizza.emit(menuPizza);
   }
 }

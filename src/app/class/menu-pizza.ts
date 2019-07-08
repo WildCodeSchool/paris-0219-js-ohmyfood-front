9a256967b7ffee2de90349f978cdf45f1f8cfcbd
Marc Bernini <bernini.marc@gmail.com>
2019-07-08 20:09:13 +0200

    Modify menuPizza class to add price in it

--- src/app/class/menu-pizza.ts
+++ src/app/class/menu-pizza.ts
@@ -6,6 +6,7 @@ export class MenuPizza {
   constructor(
     public pizza: OrderPizzas,
     public beverage: OrderBeverage,
-    public dessert: OrderDessert
+    public dessert: OrderDessert,
+    public menuPizzPrice: number
   ) {}
 }

4747994ffcbdaa43f99b84bdf8bed135dbf7a258
Marc Bernini <bernini.marc@gmail.com>
2019-07-08 19:17:58 +0200

    Create menuPizza class to create instance of it when user submit menuPizzaForm

---
+++ src/app/class/menu-pizza.ts
@@ -0,0 +1,11 @@
+import { OrderPizzas } from './order-pizzas';
+import { OrderBeverage } from './order-beverage';
+import { OrderDessert } from './order-dessert';
+
+export class MenuPizza {
+  constructor(
+    public pizza: OrderPizzas,
+    public beverage: OrderBeverage,
+    public dessert: OrderDessert
+  ) {}
+}

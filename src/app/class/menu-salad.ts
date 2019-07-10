9a8e686c3189fc125222edfa4de11e5258f19d93
Marc Bernini <bernini.marc@gmail.com>
2019-07-10 10:56:00 +0200

    Create class for salad menu

---
+++ src/app/class/menu-salad.ts
@@ -0,0 +1,12 @@
+import { OrderSalads } from './order-salads';
+import { OrderBeverage } from './order-beverage';
+import { OrderDessert } from './order-dessert';
+
+export class MenuSalad {
+  constructor(
+    public salad: OrderSalads,
+    public beverage: OrderBeverage,
+    public dessert: OrderDessert,
+    public menuSaladPrice: number
+  ) {}
+}

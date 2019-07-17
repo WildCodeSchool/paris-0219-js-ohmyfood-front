b92df1b4452c70d518d87d9cfb027127a8db2ac7
Arthur Baudel <arthurbaudel75.com>
2019-07-17 16:31:20 +0200

    basically finished tax, except I haven't tried it yet, (which is a big deal) and I haven't inserted it in admin html

--- src/app/components/admin/tax-form-admin/tax-form-admin.component.ts
+++ src/app/components/admin/tax-form-admin/tax-form-admin.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
+import { TaxService } from 'src/app/services/tax.service';
 
 @Component({
   selector: 'app-tax-form-admin',
@@ -9,9 +9,52 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class TaxFormAdminComponent implements OnInit {
 
-  constructor() { }
+  actions = ['Modifier'];
+  formCheck: FormGroup;
+  regexTax = /[0-9{1}]+[.]+[0-9]{3}/gm;
+  taxFormObject;
+  taxFormPut: FormGroup;
+  valueAction = 'Modifier';
+
+  constructor(
+    private taxService: TaxService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.initForm();
+    const getTaxObs = this.taxService.getTaxType().subscribe(data => {
+      this.taxFormObject = data;
+      getTaxObs.unsubscribe();
+    });
+  }
+
+  get FM () { return this.taxFormPut.controls; }
+
+  initForm() {
+    this.taxFormPut = this.fb.group({
+      taxName: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(45)]],
+      taxMultipliedBy: ['', [Validators.required, Validators.pattern(this.regexTax)]]
+    });
+  }
+
+  onSubmitPutForm() {
+    if (this.taxFormPut.valid) {
+      this.taxService.taxFormObject = {
+        idTaxName: this.taxFormPut.value.taxName,
+        taxValue: this.taxFormPut.value.taxMultipliedBy
+      };
+      if (confirm(`Êtes-vous certain de modifier la tax ${this.taxFormPut.value.taxName}`)) {
+        const putTaxType = this.taxService.putTaxType().subscribe(_ => {
+          const getTaxObs = this.taxService.getTaxType().subscribe(data => {
+            this.taxFormObject = data;
+            getTaxObs.unsubscribe();
+          })
+          this.taxFormPut.reset();
+          putTaxType.unsubscribe();
+        });
+      }
+    }
   }
 
 }

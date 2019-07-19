43f50941508124ec71ac92da3622b62f4a88757f
Marc Bernini <bernini.marc@gmail.com>
2019-07-19 17:50:28 +0200

    Create class to create results of details orders. This class is child of finalOrders class

---
+++ src/app/class/orders-details-admin.ts
@@ -0,0 +1,29 @@
+import { FinalOrder } from './final-order';
+import { OrderPizzas } from './order-pizzas';
+import { OrderSalads } from './order-salads';
+import { OrderBeverage } from './order-beverage';
+import { OrderDessert } from './order-dessert';
+import { MenuPizza } from './menu-pizza';
+import { MenuSalad } from './menu-salad';
+
+export class OrdersDetailsAdmin extends FinalOrder {
+  constructor(
+    public idOrder: number,
+    public dateOrder: string,
+    public clientLastName: string,
+    public clientFirstName: string,
+    public clientMail: string,
+    public clientPhoneNumber: number,
+    public userAddressFacturation: string,
+    public userDeliveryAddress: string,
+    public userMessage: string,
+    public pizza: OrderPizzas[],
+    public salad: OrderSalads[],
+    public beverage: OrderBeverage[],
+    public dessert: OrderDessert[],
+    public menuPizza: MenuPizza[],
+    public menuSalad: MenuSalad[]
+  ) {
+    super(pizza, salad, beverage, dessert, menuPizza, menuSalad);
+  }
+}

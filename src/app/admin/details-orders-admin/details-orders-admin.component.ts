1c6ebf09836437e1aa83802745a015b633f9ac5e
Marc Bernini <bernini.marc@gmail.com>
2019-07-19 14:46:19 +0200

    Create component to display orders details from database

---
+++ src/app/admin/details-orders-admin/details-orders-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-orders-admin',
+  templateUrl: './details-orders-admin.component.html',
+  styleUrls: ['./details-orders-admin.component.scss']
+})
+export class DetailsOrdersAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

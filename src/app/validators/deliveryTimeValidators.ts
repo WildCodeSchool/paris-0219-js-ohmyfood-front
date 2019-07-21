8b2ebe05ccc0abd6d21d7ad35363bdd26bdfdeb1
Marc Bernini <bernini.marc@gmail.com>
2019-07-21 13:06:58 +0200

    Add day of order because restaurant is open on tuesday to friday only at noon

--- src/app/validators/deliveryTimeValidators.ts
+++ src/app/validators/deliveryTimeValidators.ts
@@ -6,12 +6,23 @@ export function deliveryIntervalTime(orderHour: string): ValidatorFn {
     const hourMenuMin = 113000;
     const hourMenuMax = 140000;
 
-    const userHourOrder = parseInt(orderHour.split(':').join(''), 10); // Convert user hour order in number to compare it
+    const userDayOrder = orderHour.split(' ').splice(0, 1).join(''); // Get order's day
 
-    if (hourMenuMin <= userHourOrder && userHourOrder <= hourMenuMax) {
+    const userHourOrder = orderHour.split(' ').splice(1, 1).join(''); // Get order's hour
+
+    const finalHourOrder = parseInt(userHourOrder.split(':').join(''), 10); // Convert user hour order in number to compare it
+
+    if (
+      hourMenuMin <= finalHourOrder &&
+      finalHourOrder <= hourMenuMax &&
+      userDayOrder !== 'Monday' &&
+      userDayOrder !== 'Saturday' &&
+      userDayOrder !== 'Sunday'
+      ) {
         return null;
     } else {
-        return { notInTime: 'Nos menus ne sont disponibles que le midi, sur nos horaires d\'ouvertures, entre 11h30 et 14h' }
+        return { notInTime: `Nos menus ne sont disponibles que le midi,
+                du mardi au vendredi, sur nos horaires d\'ouvertures, entre 11h30 et 14h` };
     }
   };
 }

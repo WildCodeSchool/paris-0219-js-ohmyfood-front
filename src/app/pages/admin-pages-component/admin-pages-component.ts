a10765a65e74d9167a87f02c3cbabd7299af28de
Marc Bernini <bernini.marc@gmail.com>
2019-07-22 13:24:28 +0200

    Regroup all component for admin in admin page. Add three buttons to display component according to admin choice

--- src/app/pages/admin-pages-component/admin-pages-component.ts
+++ src/app/pages/admin-pages-component/admin-pages-component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPagesComponent implements OnInit {
 
+  forms = true;
+  ordersDetails: boolean;
+  ordersArchived: boolean;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeComponent(componentToDisplay: string) {
+    switch (componentToDisplay) {
+      case 'forms':
+        this.forms = true;
+        this.ordersDetails = false;
+        this.ordersArchived = false;
+        break;
+
+      case 'ordersDetails':
+        this.forms = false;
+        this.ordersDetails = true;
+        this.ordersArchived = false;
+        break;
+
+      case 'ordersArchived':
+        this.forms = false;
+        this.ordersDetails = false;
+        this.ordersArchived = true;
+        break;
+      default: null;
+    }
   }
 
 }

afa8e4f61bcaa098ceacc3bf63570330964e218e
Marc Bernini <bernini.marc@gmail.com>
2019-07-22 16:09:03 +0200

    Create new component to display orders archived

---
+++ src/app/components/admin/details-orders-archived-admin/details-orders-archived-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-orders-archived-admin',
+  templateUrl: './details-orders-archived-admin.component.html',
+  styleUrls: ['./details-orders-archived-admin.component.scss']
+})
+export class DetailsOrdersArchivedAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

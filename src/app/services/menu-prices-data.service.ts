c1e94ae05df0ab8e9f0845d1af81962249bdf77b
Marc Bernini <bernini.marc@gmail.com>
2019-07-23 04:00:13 +0200

    Update service with new argument to avoid error

--- src/app/services/menu-prices-data.service.ts
+++ src/app/services/menu-prices-data.service.ts
@@ -83,7 +83,7 @@ export class MenuPricesDataService {
   }
 
   // Get choice of user in menu
-  getRadioButton(form: FormGroup, index: number, menusPrice: object, check: string[]) {
+  getRadioButton(form: FormGroup, index: number, menusPrice: object, check: string[], menuType: string) {
     if (check[0] === 'idPizzas') {
       const length = form.controls.pizza[`controls`].length; // To get array length
 
@@ -103,13 +103,14 @@ export class MenuPricesDataService {
           form.controls.beverage[`controls`][i].value.bevQuantity = 1;
 
           // Patch value if user pick Beer
-          if (index === i && form.controls.beverage[`controls`][i].value.bevName === 'Bière') {
+          if (index === i && form.controls.beverage[`controls`][i].value.bevName === 'Bière' && menuType === 'menuPizza') {
             form.controls.pizzaMenuPriceTotal.patchValue(
               (Number.parseInt(form.controls.pizzaMenuPriceTotal.value, 10) + 1).toFixed(2)
               );
 
             // If user didn't pick beer, patch value with original price
-          } else {
+          } else if (menuType === 'menuPizza') {
+
             form.controls.pizzaMenuPriceTotal.patchValue(
               menusPrice[0].menuPizzPrice.toFixed(2)
               );
